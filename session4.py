b60e7ffc4ad8c6cae42b0fc0fe198996c75a29af
Alfredo171208 <alfredochano06@gmail.com>
2024-09-29 18:53:37 +0700

    updated

---
+++ session4.py
@@ -0,0 +1,16 @@
+def list():
+    list1 = [1,2,3,4,5]
+    list2 = [5,6,7,8,9]
+    list3 = set(list1 + list2)
+    return list3
+
+alex = list()
+print (alex)
+
+def alex (number, letter):
+    test = ''.join([char * num for char, num in zip(letter, number)])
+    
+    return test
+
+print(alex((1,2,3,4), 'alex'))
+
