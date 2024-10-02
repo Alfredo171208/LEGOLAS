148812d8f42ff42f9cc8c6054f16d333f1074fc0
Alfredo171208 <alfredochano06@gmail.com>
2024-10-02 22:52:59 +0700

    updated

--- session4.py
+++ session4.py
@@ -1,3 +1,4 @@
+
 def list():
     list1 = [1,2,3,4,5]
     list2 = [5,6,7,8,9]
@@ -12,5 +13,6 @@ def alex (number, letter):
     
     return test
 
-print(alex((1,2,3,4), 'alex'))
+print(alex((1,2,3,4), 'reha'))
+
 
